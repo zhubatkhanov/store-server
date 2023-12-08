d1ef8de71f2bbb4d977c612d0102aa2dfa7ed5bc
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-09 00:33:13 +0500

    add products function

--- store/products/views.py
+++ store/products/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'products/index.html')
+
+def products(request):
+    return render(request, 'products/products.html')
