7b69d75885482b97a52b18c5ca67adbe79bed1c6
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-12 23:39:30 +0500

    add html-parent file, and use context

--- store/products/views.py
+++ store/products/views.py
@@ -3,7 +3,33 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Store',
+    }
+    return render(request, 'products/index.html', context=context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                "image": "/static/vendor/img/products/Adidas-hoodie.png",
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': '6 090,00 руб.',
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                "image": "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
+                'name': 'Синяя куртка The North Face',
+                'price': '23 725,00 руб.',
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+            },
+            {
+                "image": "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': '3 390,00 руб.',
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            }
+        ]
+    }
+    return render(request, 'products/products.html', context=context)
