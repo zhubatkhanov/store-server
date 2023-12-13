e3b02b108337274a44e02ac24b4d789165b1159e
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-14 01:12:32 +0500

    working with models

--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product, ProductCategory
 
 
 # Create your views here.
@@ -11,25 +12,7 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Каталог',
-        'products': [
-            {
-                "image": "/static/vendor/img/products/Adidas-hoodie.png",
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': '6 090,00 руб.',
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-            },
-            {
-                "image": "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
-                'name': 'Синяя куртка The North Face',
-                'price': '23 725,00 руб.',
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-            },
-            {
-                "image": "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': '3 390,00 руб.',
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-            }
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context=context)
