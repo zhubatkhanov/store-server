dcfac4af3a1a5cdcf28e9551825209631dc4ec54
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-17 16:14:33 +0500

    add namespace in url

---
+++ store/products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('products/', views.products, name = 'products'),
+]
--- store/store/urls.py
+++ store/store/urls.py
@@ -14,15 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from products.views import index, products
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name = 'index'),
-    path('products/', products, name = 'products'),
+    path('', include('products.urls', namespace='products'))
 ]
 
 if settings.DEBUG:
