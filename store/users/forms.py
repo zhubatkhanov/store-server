19fd076d52204a173b2755483a5dcded6e60ee09
zhubatkhanov <zhubatkhanovsanzhar@gmail.com>
2023-12-19 15:24:41 +0500

    stylize form

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,8 +1,17 @@
 from .models import User
 from django.contrib.auth.forms import AuthenticationForm
+from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите имя пользователя',
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите пароль',
+    }))
     class Meta:
         model = User
         fields = ('username', 'password')
